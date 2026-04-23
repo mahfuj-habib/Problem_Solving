34ba31810557c0854486b803a6f302d9d61e816f
mahfujhabibmahi <mahfujhabibmahiprodhan@gmail.com>
2026-04-23 16:23:12 +0600

    Daily Practise Implementation -02

---
+++ alternatingString.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(string s)
+{
+    int i=0;
+    int j=0;
+    for(int i=0;i<s.length();i++)
+    {
+        
+    }
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        solve(s);
+    }
+}
