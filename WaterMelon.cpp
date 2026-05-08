a2abc77dcde17ee5ac54df8a00c858a37b43c903
mahfujhabibmahi <mahfujhabibmahiprodhan@gmail.com>
2026-05-08 20:19:21 +0600

    Daily Problem Implementation 01

---
+++ WaterMelon.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int w;
+    cin>>w;
+    if(w==2)
+    {
+        cout<<"NO";
+        return 0;
+    }
+    else if(w%2==0)
+    {
+        cout<<"YES";
+    }
+    else
+    cout<<"NO";
+}
